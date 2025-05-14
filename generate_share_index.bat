ebaf25fc9ff42becdfd8a8ce75b65eae207cf1b8
TheRealOrco <orcoh89@gmail.com>
2025-05-14 19:36:01 +0300

    Add batch script and GitHub Action to auto-update share index

---
+++ generate_share_index.bat
@@ -0,0 +1,37 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+set "folder=share"
+set "output=%folder%\index.html"
+
+rem Check if the folder exists
+if not exist "%folder%" (
+    echo Error: Folder "%folder%" does not exist.
+    pause
+    exit /b 1
+)
+
+> "%output%" (
+    echo ^<html^>
+    echo ^<head^>
+    echo     ^<title^>Shared Files^</title^>
+    echo ^</head^>
+    echo ^<body^>
+    echo     ^<h1^>Files in /share/^</h1^>
+    echo     ^<ul^>
+
+    for %%F in ("%folder%\*") do (
+        if /I not "%%~nxF"=="index.html" (
+            if not "%%~aF"=="d" (
+                echo         ^<li^>^<a href="%%~nxF"^>%%~nxF^</a^>^</li^>
+            )
+        )
+    )
+
+    echo     ^</ul^>
+    echo ^</body^>
+    echo ^</html^>
+)
+
+echo Done: %output% generated.
+pause
